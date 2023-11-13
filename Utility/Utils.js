2c5f48d06cd3a11859e5496654783204cf3603cb
Dominique <dominique@kedokagroup.com>
2023-11-14 00:53:02 +0200

    updated s3 region

--- Utility/Utils.js
+++ Utility/Utils.js
@@ -238,9 +238,9 @@ exports.uploadBase64ToS3 = async (
     imageType = 'jpeg'
 ) => {
     const s3 = new AWS.S3({
+        region: 'us-west-1',
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: 'us-east-1',
     });
 
     try {
