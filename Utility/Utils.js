5f664312fd52876ac39dba1bf2094a5b0de26fd8
Dominique <dominique@kedokagroup.com>
2023-11-14 00:53:17 +0200

    updated s3 region

--- Utility/Utils.js
+++ Utility/Utils.js
@@ -14,7 +14,7 @@ const { logger } = require('../LogService');
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-1',
+    region: 'us-west-1',
 });
 
 dayjs.extend(utc);
