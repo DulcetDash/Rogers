5024a27c442bb200bfee9083a23038f2a6024378
Dominique <dominique@kedokagroup.com>
2023-11-16 20:41:47 +0200

    Added adjusted price to catalogue model and few lints

--- models/CatalogueModel.js
+++ models/CatalogueModel.js
@@ -1,84 +1,85 @@
-const dynamoose = require("dynamoose");
+const dynamoose = require('dynamoose');
 
 const catalogueSchema = new dynamoose.Schema(
-  {
-    id: {
-      type: String,
-      hashKey: true,
+    {
+        id: {
+            type: String,
+            hashKey: true,
+        },
+        shop_fp: {
+            type: String,
+            required: true,
+            index: {
+                global: true,
+                rangeKey: 'id',
+                name: 'shopfp-index',
+                project: true,
+                throughput: 'ON_DEMAND',
+            },
+        },
+        brand: {
+            type: String,
+            required: true,
+            index: {
+                global: true,
+                rangeKey: 'id',
+                name: 'brand-index',
+                project: true,
+                throughput: 'ON_DEMAND',
+            },
+        },
+        product_name: String,
+        product_price: String,
+        priceAdjusted: Number,
+        product_picture: dynamoose.type.ANY,
+        sku: {
+            type: String,
+            required: true,
+            index: {
+                global: true,
+                rangeKey: 'id',
+                name: 'sku-index',
+                project: true,
+                throughput: 'ON_DEMAND',
+            },
+        },
+        used_link: String,
+        category: {
+            type: String,
+            required: true,
+            index: {
+                global: true,
+                rangeKey: 'id',
+                name: 'category-index',
+                project: true,
+                throughput: 'ON_DEMAND',
+            },
+        },
+        subcategory: String,
+        shop_name: {
+            type: String,
+            required: true,
+            index: {
+                global: true,
+                rangeKey: 'id',
+                name: 'shopname-index',
+                project: true,
+                throughput: 'ON_DEMAND',
+            },
+        },
+        website_link: String,
+        description: String,
     },
-    shop_fp: {
-      type: String,
-      required: true,
-      index: {
-        global: true,
-        rangeKey: "id",
-        name: "shopfp-index",
-        project: true,
-        throughput: "ON_DEMAND",
-      },
-    },
-    brand: {
-      type: String,
-      required: true,
-      index: {
-        global: true,
-        rangeKey: "id",
-        name: "brand-index",
-        project: true,
-        throughput: "ON_DEMAND",
-      },
-    },
-    product_name: String,
-    product_price: String,
-    product_picture: dynamoose.type.ANY,
-    sku: {
-      type: String,
-      required: true,
-      index: {
-        global: true,
-        rangeKey: "id",
-        name: "sku-index",
-        project: true,
-        throughput: "ON_DEMAND",
-      },
-    },
-    used_link: String,
-    category: {
-      type: String,
-      required: true,
-      index: {
-        global: true,
-        rangeKey: "id",
-        name: "category-index",
-        project: true,
-        throughput: "ON_DEMAND",
-      },
-    },
-    subcategory: String,
-    shop_name: {
-      type: String,
-      required: true,
-      index: {
-        global: true,
-        rangeKey: "id",
-        name: "shopname-index",
-        project: true,
-        throughput: "ON_DEMAND",
-      },
-    },
-    website_link: String,
-    description: String,
-  },
-  {
-    // timestamps: true,
-    saveUnknown: true,
-  }
+    {
+        // timestamps: true,
+        saveUnknown: true,
+    }
 );
 
-module.exports = dynamoose.model("Catalogues", catalogueSchema, {
-  throughput: "ON_DEMAND",
-  update: false,
-  waitForActive: true,
-  initialize: true,
-  create: true,
+module.exports = dynamoose.model('Catalogues', catalogueSchema, {
+    throughput: 'ON_DEMAND',
+    update: false,
+    waitForActive: true,
+    initialize: true,
+    create: true,
 });
